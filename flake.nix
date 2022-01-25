3fd70070334a6fdce422875cd7aa7929c27402be
David Pätzel <david.paetzel@uni-a.de>
2022-01-25 14:33:09 +0100

    :arrow_up: Update deps, fix numpy version issues

--- flake.nix
+++ flake.nix
@@ -1,22 +1,25 @@
 {
-  description = "Evaluation scripts for the paper BERBL implementation paper";
+  description = "Evaluation scripts for the BERBL implementation paper";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  # 2022-01-24
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/8ca77a63599ed951d6a2d244c1d62092776a3fe1";
   # inputs.berbl-eval.url = "TODO"
   inputs.berbl-eval = {
     type = "path";
-    path = "/home/david/Projekte/berbl-experiments/berbl-eval";
+    path = "/home/david/Projekte/berbl/berbl-eval";
   };
 
-  outputs = { self, nixpkgs, berbl-eval }: {
+  outputs = { self, nixpkgs, berbl-eval }:
+    with import nixpkgs { system = "x86_64-linux"; };
+    let python = python39;
+    in rec {
 
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      mkShell {
+      devShell.x86_64-linux = mkShell {
         packages = [
-          (python3.withPackages
+          (python.withPackages
             (ps: [ berbl-eval.defaultPackage.x86_64-linux ps.networkx ]))
         ];
       };
-  };
+    };
 }
